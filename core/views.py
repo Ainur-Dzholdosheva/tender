9bd7c16c1ad630d8f9c24f98da3d96987a7b6603
Ainur-Dzholdosheva <ajnurdzoldoseva@gmail.com>
2021-07-31 18:19:26 +0600

    added viewsets and routers

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TenderSerializer
+from .models import Tender
+
+class TenderViewSet(viewsets.ModelViewSet):
+    queryset = Tender.objects.all().order_by('name')
+    serializer_class = TenderSerializer
