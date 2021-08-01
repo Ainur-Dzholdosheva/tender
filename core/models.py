6bad21016efdb9b98a917052461681ece0da23ef
Ainur-Dzholdosheva <ajnurdzoldoseva@gmail.com>
2021-08-01 14:16:21 +0600

    added price, number and participants to models.py in tender

--- core/models.py
+++ core/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class Tender(models.Model):
+    price=models.CharField(max_length=255, null=True, blank=True)
+    number=models.CharField(max_length=255, null=True, blank=True)
     name=models.CharField(max_length=60)
     description=models.TextField(max_length=255)
+    buyer=models.CharField(max_length=255, null=True, blank=True)
+    participant=models.CharField(max_length=255, null=True, blank=True)
+    
 
 
     def __str__(self):
